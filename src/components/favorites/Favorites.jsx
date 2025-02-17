98135277937537a5685139b9312525d7932bbba7
alixanov <alixonovshukurullo13@gmail.com>
2025-02-17 21:11:25 +0500

    138

--- src/components/favorites/Favorites.jsx
+++ src/components/favorites/Favorites.jsx
@@ -1,133 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import img1 from '../../assets/swiper-img/1.png';
-import img2 from '../../assets/swiper-img/2.jpg';
-import img3 from '../../assets/swiper-img/3.jpg';
-import img4 from '../../assets/swiper-img/4.jpg';
-import img5 from '../../assets/swiper-img/5.png';
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-cards";
 
+import CloseIcon from "@mui/icons-material/Close";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+import img1 from "../../assets/swiper-img/1.png";
+import img2 from "../../assets/swiper-img/2.jpg";
+import img3 from "../../assets/swiper-img/3.jpg";
+import img4 from "../../assets/swiper-img/4.jpg";
+import img5 from "../../assets/swiper-img/5.png";
 
 import "./favorites.css";
-import CloseIcon from '@mui/icons-material/Close';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 
-const initialGirls = [
-     { id: 1, name: 'Ника', img: img1, age: 36, city: 'Москва', status: 'Свободная', height: 166 },
-     { id: 2, name: 'Алиса', img: img2, age: 24, city: 'СПб', status: 'Свободная', height: 170 },
-     { id: 3, name: 'Мария', img: img3, age: 27, city: 'Казань', status: 'Свободная', height: 165 },
-     { id: 4, name: 'София', img: img4, age: 22, city: 'Екатеринбург', status: 'Свободная', height: 173 },
-     { id: 5, name: 'Анна', img: img5, age: 29, city: 'Новосибирск', status: 'Свободная', height: 168 },
+const girls = [
+     { id: 1, name: "Ника", img: img1, age: 36, city: "Москва", status: "Свободная", height: 166 },
+     { id: 2, name: "Алиса", img: img2, age: 24, city: "СПб", status: "Свободная", height: 170 },
+     { id: 3, name: "Мария", img: img3, age: 27, city: "Казань", status: "Свободная", height: 165 },
+     { id: 4, name: "София", img: img4, age: 22, city: "Екатеринбург", status: "Свободная", height: 173 },
+     { id: 5, name: "Анна", img: img5, age: 29, city: "Новосибирск", status: "Свободная", height: 168 },
 ];
 
 const Favorites = () => {
-     const [currentIndex, setCurrentIndex] = useState(0);
-     const [isTransitioning, setIsTransitioning] = useState(false);
-     const [animationClass, setAnimationClass] = useState('appear');
-     const [showButton, setShowButton] = useState(null);
-     const [touchStartX, setTouchStartX] = useState(null); // Начальная позиция касания
-     const [isMobile, setIsMobile] = useState(false); // Для проверки мобильного устройства
+     const swiperRef = useRef(null);
+     const [action, setAction] = useState(null); // Состояние для отслеживания действия
 
-     useEffect(() => {
-          // Определяем, мобильное устройство или нет
-          const checkIsMobile = () => {
-               setIsMobile(window.innerWidth <= 768);
-          };
-          checkIsMobile();
-          window.addEventListener('resize', checkIsMobile);
-          return () => window.removeEventListener('resize', checkIsMobile);
-     }, []);
-
-     const handleTransition = (direction, buttonType) => {
-          if (isTransitioning) return;
-          setIsTransitioning(true);
-          setAnimationClass(`transitioning-${direction}`);
-          setShowButton(buttonType);
-
+     const swipeLeft = () => {
+          setAction("dislike");
           setTimeout(() => {
-               setCurrentIndex((prevIndex) => {
-                    if (direction === 'left') {
-                         return (prevIndex - 1 + initialGirls.length) % initialGirls.length;
-                    } else {
-                         return (prevIndex + 1) % initialGirls.length;
-                    }
-               });
-               setAnimationClass('appear');
-               setShowButton(null);
-               setTimeout(() => {
-                    setIsTransitioning(false);
-               }, 400);
-          }, 400);
+               if (swiperRef.current) swiperRef.current.swiper.slideNext();
+               setAction(null); // Сброс состояния после свайпа
+          }, 300); // Задержка для отображения текста перед свайпом
      };
 
-     const handleTouchStart = (e) => {
-          setTouchStartX(e.touches[0].clientX);
-     };
-
-     const handleTouchEnd = (e) => {
-          if (touchStartX === null) return;
-
-          const touchEndX = e.changedTouches[0].clientX;
-          const deltaX = touchEndX - touchStartX;
-
-          // Проверяем, мобильный режим или нет
-          if (isMobile) {
-               if (deltaX > 50) {
-                    // Пропускаем анкету (вправо) без действия
-                    handleTransition('left', null);
-               } else if (deltaX < -50) {
-                    // Пропускаем анкету (влево) без действия
-                    handleTransition('right', null);
-               }
-          } else {
-               // На десктопе свайп вправо - дизлайк, влево - лайк
-               if (deltaX > 50) {
-                    handleTransition('left', 'cross'); // Десктоп: дизлайк
-               } else if (deltaX < -50) {
-                    handleTransition('right', 'heart'); // Десктоп: лайк
-               }
-          }
-
-          setTouchStartX(null);
+     const swipeRight = () => {
+          setAction("like");
+          setTimeout(() => {
+               if (swiperRef.current) swiperRef.current.swiper.slideNext();
+               setAction(null); // Сброс состояния после свайпа
+          }, 300); // Задержка для отображения текста перед свайпом
      };
 
      return (
           <div className="heart__container">
-               <div className="heart__card">
-                    <div
-                         className={`image-container ${animationClass}`}
-                         onTouchStart={handleTouchStart}
-                         onTouchEnd={handleTouchEnd}
-                    >
-                         <img
-                              src={initialGirls[currentIndex].img}
-                              alt={initialGirls[currentIndex].name}
-                         />
-
-                         {showButton && (
-                              <div className={`button-overlay ${showButton === 'heart' ? 'heart' : 'cross'}`}>
-                                   {showButton === 'heart' ? (
-                                        <FavoriteIcon sx={{ fontSize: "80px" }} />
-                                   ) : (
-                                        <CloseIcon sx={{ fontSize: "80px" }} />
-                                   )}
+               <Swiper
+                    effect={"cards"}
+                    grabCursor={true}
+                    modules={[EffectCards]}
+                    className="heart__card"
+                    ref={swiperRef}
+                    loop={true}
+               >
+                    {girls.map((girl) => (
+                         <SwiperSlide key={girl.id} className="image-container">
+                              <img src={girl.img} alt={girl.name} />
+                              {action && (
+                                   <div className={`swipe-text ${action}`}>
+                                        {action === "like" ? <FavoriteIcon sx={{ fontSize: 60 }} /> : <CloseIcon sx={{ fontSize: 60 }} />}
+                                   </div>
+                              )}
+                              <div className="heart__txt__sli">
+                                   <p>{girl.name}, {girl.age}</p>
+                                   <span>{girl.city}</span>
+                                   <span>{girl.status}</span>
+                                   <span>Рост: {girl.height} см</span>
                               </div>
-                         )}
+                         </SwiperSlide>
+                    ))}
+               </Swiper>
 
-                         <div className="heart__txt__sli">
-                              <p>{initialGirls[currentIndex].name}, {initialGirls[currentIndex].age}</p>
-                              <span>{initialGirls[currentIndex].city}</span>
-                              <span>{initialGirls[currentIndex].status}</span>
-                              <span>Рост: {initialGirls[currentIndex].height} см</span>
-                         </div>
-
-                         <div className="btn-swip">
-                              <button className="swipe-rightt" onClick={() => handleTransition('left', 'cross')}>
-                                   <CloseIcon sx={{ fontSize: "45px" }} className='btn__left' />
-                              </button>
-                              <button className="swipe-leftt" onClick={() => handleTransition('right', 'heart')}>
-                                   <FavoriteIcon sx={{ fontSize: "45px" }} className='btn__right' />
-                              </button>
-                         </div>
-                    </div>
+               {/* Кнопки лайка и дизлайка */}
+               <div className="btn-swip">
+                    <button className="btn__left" onClick={swipeLeft}>
+                         <CloseIcon sx={{ fontSize: "45px" }} />
+                    </button>
+                    <button className="btn__right" onClick={swipeRight}>
+                         <FavoriteIcon sx={{ fontSize: "45px" }} />
+                    </button>
                </div>
           </div>
      );
